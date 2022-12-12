6f6fa32026e22926620d96c269abd2fd3385c31c
islamabdelrahman <islam.abdelrahman00@gmail.com>
2022-12-12 01:56:31 +0100

    adding router back

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import router from '@/router/router.js'
 
 createApp(App).use(router).mount('#app')
